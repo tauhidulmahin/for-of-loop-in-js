c5ef28892031e226766d78878c16b1503c3afacc
tauhidul-islam56 <tauhidulmahin@gmail.com>
2024-04-29 13:58:59 +0600

    initial commit

---
+++ country-json.js
@@ -0,0 +1,23 @@
+let countries = [
+ 
+    {name: 'Afghanistan', code: 'AF'}, 
+    {name: 'Åland Islands', code: 'AX'}, 
+    {name: 'Albania', code: 'AL'}, 
+    {name: 'Algeria', code: 'DZ'}, 
+    {name: 'American Samoa', code: 'AS'}, 
+    {name: 'AndorrA', code: 'AD'}, 
+    {name: 'Angola', code: 'AO'}, 
+    {name: 'Anguilla', code: 'AI'}, 
+    {name: 'Antarctica', code: 'AQ'}, 
+    {name: 'Antigua and Barbuda', code: 'AG'}
+]    
+
+// console.log(countries[6].code);
+// console.log(countries[3].name);
+// console.log(countries[9].code);
+// console.log(countries[2].name);
+
+for (const country of countries) {
+    console.log(country);
+}
+
---
+++ test-data-json.js
@@ -0,0 +1,43 @@
+let tests = [    //array
+    {    //array[0]
+    "id": 1,
+    "first_name": "Jeanette",
+    "last_name": "Penddreth",
+    "email": "jpenddreth0@census.gov",
+    "gender": "Female",
+    "ip_address": "26.58.193.2"
+    }, 
+    {    //array[1]
+    "id": 2,
+    "first_name": "Giavani",
+    "last_name": "Frediani",
+    "email": "gfrediani1@senate.gov",
+    "gender": "Male",
+    "ip_address": "229.179.4.212"
+    }, 
+    {    //array[2]
+    "id": 3,
+    "first_name": "Noell",
+    "last_name": "Bea",
+    "email": "nbea2@imageshack.us",
+    "gender": "Female",
+    "ip_address": "180.66.162.255"
+    }, 
+    {    //array[3]
+    "id": 4,
+    "first_name": "Willard",
+    "last_name": "Valek",
+    "email": "wvalek3@vk.com",
+    "gender": "Male",
+    "ip_address": "67.76.188.26"
+  }
+]
+
+// console.log(tests[0].id);
+// console.log(tests[1].first_name);
+// console.log(tests[2].last_name);
+// console.log(tests[3].gender);
+
+for (const test of tests) {
+  console.log(test);
+}
